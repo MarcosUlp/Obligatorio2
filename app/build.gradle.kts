278f712396898e14a8f126d1240696345e9f1aec
Marcos Giraudi <crinomarcos@gmail.com>
2025-09-08 00:12:18 -0300

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,11 +35,12 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
